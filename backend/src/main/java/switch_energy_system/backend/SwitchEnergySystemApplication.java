eaf3a4da2c8bc12d242588f08bbc457e74209c2d
Keerthick-S <mail2keerthick@gmail.com>
2023-02-10 11:42:47 +0530

    initial commit --backend

---
+++ backend/src/main/java/switch_energy_system/backend/SwitchEnergySystemApplication.java
@@ -0,0 +1,13 @@
+package switch_energy_system.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchEnergySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwitchEnergySystemApplication.class, args);
+	}
+
+}
